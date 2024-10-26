9cf2b7ef9b76eac0f8b83b5cb6c14f8620f20a81
malripo <114119856+malripo@users.noreply.github.com>
2024-10-26 22:22:59 +0900

    gmail setup

--- a_core/settings.py
+++ a_core/settings.py
@@ -219,7 +219,19 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "/"
 
 # for email authentication
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+if ENVIRONMENT == "production" or POSTGRES_LOCALLY == True:    
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = env('EMAIL_ADDRESS')
+    EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+    # DEFAULT_FROM_EMAIL = 'Awesome'
+    DEFAULT_FROM_EMAIL = f'Awesome {env("EMAIL_ADDRESS")}'
+    ACCOUNT_EMAIL_SUBJECT_PREFIX = ''
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_REQUIRED = True
 
